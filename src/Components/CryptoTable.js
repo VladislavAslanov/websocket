bf1ec121c178694d44fa8271b7379503a5dcf33b
Vladislav Aslanov <50210559+VladislavAslanov@users.noreply.github.com>
2022-06-14 14:54:36 +0200

    Cycle for returns a list of objects with nessecary data

--- src/Components/CryptoTable.js
+++ src/Components/CryptoTable.js
@@ -4,11 +4,27 @@ class CryptoTable extends Component {
 constructor() {
   super();
 
-  this.state = {};
+  this.state = {
+    cryptoItem: {
+      Rank: null,
+      Symbol: null,
+      Title: null,
+      Price: null,
+      Changing: null,
+    }
+  };
 
 };
 
 componentDidMount() {
+  let item = {
+    Rank: null,
+    Symbol: null,
+    Title: null,
+    Price: null,
+    Changing: null,
+  }
+
   const xhr = new XMLHttpRequest()
 
   xhr.open("get", "https://api.coincap.io/v2/assets")
@@ -17,16 +33,24 @@ componentDidMount() {
   xhr.addEventListener("readystatechange", function () {
       if (this.readyState === 4 && this.status === 200) {
         let result = JSON.parse(this.responseText);
-        console.log(result);
-      }
+          for(let i = 0; i < result.data.length; i++) {
+            item = {
+              Rank: result.data[i].rank,
+              Symbol: result.data[i].symbol,
+              Title: result.data[i].name,
+              Price: result.data[i].priceUsd,
+              Changing: result.data[i].changePercent24Hr,
+            }
+          }
+         }
+        }
+      )
     }
-  )
-}
 
   render() {
     return (
       <div>
-
+        {this.state.cryptoItem.Rank}
       </div>
     )
   }
