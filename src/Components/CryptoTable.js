6230cca42fe1d8e94b6725cc0f96819ccc466996
Vladislav Aslanov <50210559+VladislavAslanov@users.noreply.github.com>
2022-06-17 17:12:47 +0200

    List of cryptocurrencies now is visible

--- src/Components/CryptoTable.js
+++ src/Components/CryptoTable.js
@@ -1,21 +1,17 @@
-import React, { Component } from 'react';
+import React, {useEffect, useState} from 'react';
+import List from "./List";
 
-class CryptoTable extends Component {
-constructor() {
-  super();
-
-  this.state = {
-    cryptoItem: {
+const CryptoTable = () => {
+  const [cryptoItem, setCryptoItem] = useState({
       Rank: null,
       Symbol: null,
       Title: null,
       Price: null,
-      Changing: null,
-    }
-  };
-};
+      Changing: null
+    });
+  const [resultState, setResultState] = useState([])
 
-componentDidMount() {
+useEffect(() =>  {
   let item = {
     Rank: null,
     Symbol: null,
@@ -25,37 +21,34 @@ componentDidMount() {
   };
 
   let itemList = [];
-
   const xhr = new XMLHttpRequest()
 
-  xhr.open("get", "https://api.coincap.io/v2/assets")
+  xhr.onreadystatechange = async function () {
+    if(xhr.readyState === 4 && xhr.status === 200) {
+      let result = JSON.parse(this.responseText);
+      for(let i = 0; i < result.data.length; i++) {
+        item = {
+          Rank: result.data[i].rank,
+          Symbol: result.data[i].symbol,
+          Title: result.data[i].name,
+          Price: result.data[i].priceUsd,
+          Changing: result.data[i].changePercent24Hr,
+        }
+        itemList.push(item);
+      }
+      setResultState(itemList)
+    }
+  }
+  xhr.open("get", "https://api.coincap.io/v2/assets");
   xhr.send();
+  }, [])
 
-  xhr.addEventListener("readystatechange", function () {
-      if (this.readyState === 4 && this.status === 200) {
-        let result = JSON.parse(this.responseText);
-        for(let i = 0; i < result.data.length; i++) {
-          item = {
-              Rank: result.data[i].rank,
-              Symbol: result.data[i].symbol,
-              Title: result.data[i].name,
-              Price: result.data[i].priceUsd,
-              Changing: result.data[i].changePercent24Hr,
-              }
-          itemList.push(item);
-            }
-        }
-        }
-      )
 
-}
-
-  render() {
     return (
       <div className={'container'}>
-        <h1 className={'heading'}>Table of cryptocurrencies</h1>
+        <h1 className={'heading'}>Tabulka kryptoměn</h1>
         <form action="">
-          <select name="sort" id="sort" className={'sort'}>
+          <select name="sort" id="sort" className={'sortButton'}>
             <option value="rank">Rank</option>
             <option value="symbol">Zkratka</option>
             <option value="title">Název</option>
@@ -65,16 +58,11 @@ componentDidMount() {
         </form>
         <div className={'table'}>
           <div className={'parameters'}>
-            <div className={'parameters_item'}>Rank</div>
-            <div className={'parameters_item'}>Zkratka</div>
-            <div className={'parameters_item'}>Název</div>
-            <div className={'parameters_item'}>Cena</div>
-            <div className={'parameters_item'}>Změna 24h</div>
+            {resultState.sort((a, b) => +a.Rank < +b.Rank ? -1 : 1).map((res, index) => <List key={index} {...res} />)}
           </div>
         </div>
       </div>
     )
-  }
 }
 
 export default CryptoTable;
